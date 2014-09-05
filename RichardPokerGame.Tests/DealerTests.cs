de9c18221c2b2cae0f0460cb0a3eeb6b51014a64
Richard Fonseca <rfonseca@ipcoop.com>
2014-09-04 22:52:38 -0400

    Added first test Dealer_Should_Have_52_Cards made pass

---
+++ RichardPokerGame.Tests/DealerTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using RichardPokerGame;
+using RichardPokerGame.Poker;
+
+namespace RichardPokerGame.Tests
+{
+    [TestFixture]
+    public class DealerTests
+    {
+        [Test]
+        public void Dealer_Should_Have_52_Cards()
+        {
+            //Arrange
+            const int cardCount = 52;
+
+            //Act
+            var dealer = new Dealer();
+            
+            //Assert    
+            Assert.AreEqual(cardCount, dealer.CardCount);            
+        }
+
+    }
+}
---
+++ RichardPokerGame/Poker/Dealer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RichardPokerGame.Poker
+{
+    public class Dealer
+    {
+                       
+        public int CardCount { get; set; }       
+
+        public Dealer()
+        {            
+            CardCount = 52;
+        }
+    }
+}

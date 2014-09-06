94ef4b28eab37ddcbc06330eb2b7440baaa40983
Richard Fonseca <rfonseca@ipcoop.com>
2014-09-06 10:47:13 -0400

    refactored IsStraightFlush

--- RichardPokerGame/Poker/PokerEvaluator.cs
+++ RichardPokerGame/Poker/PokerEvaluator.cs
@@ -7,9 +7,25 @@ namespace RichardPokerGame.Poker
 {
     public static class PokerEvaluator
     {
+
         public static bool IsStraightFlush(List<Card> cards)
         {
-            return true;
+
+            //first check if all suits are the same
+            if (cards.Count(c => c.Suit == cards[0].Suit) == 5)
+            {
+                //sort cards...this should be moved if going to pass more cards, but leave here for now
+                var sortedCards = from card in cards
+                                  orderby card.Value
+                                  select card;
+
+                bool isConsecutive = !sortedCards.Select((i, j) => i.Value - j).Distinct().Skip(1).Any();
+
+                return isConsecutive;
+            }
+                                                               
+
+            return false;          
         }
     }
 }
